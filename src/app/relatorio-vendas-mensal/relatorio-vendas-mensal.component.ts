91acc46ba5d468914f77cb97499b9d733f3e897a
uniubeAVA <mobile@uniube.br>
2021-03-02 20:09:37 -0300

    oitavo commit

--- src/app/relatorio-vendas-mensal/relatorio-vendas-mensal.component.ts
+++ src/app/relatorio-vendas-mensal/relatorio-vendas-mensal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'node_modules/chart.js'
+import { Usuario } from 'src/Classes/Usuario';
+import { RemoteService } from 'src/providers/remote.service';
 
 @Component({
   selector: 'app-relatorio-vendas-mensal',
@@ -7,9 +10,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RelatorioVendasMensalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private remote: RemoteService) { }
 
-  ngOnInit(): void {
+  public respostaData = {
+    aceitas: '',
+    recusadas: '',
+    feitas: '',
+    entregues: '',
+    totalVendidos: ''
+  };
+
+  async ngOnInit(){
+    const monthNames = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+      "Julho", "Augosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    ];
+
+    const d = new Date();
+
+    let obj = {
+      mes: d.getMonth()+1,
+      ano: d.getFullYear()
+    }
+    let url = Usuario.URL+'encomendaInformacao/buscarEncomendaInformacaoRelatorio';
+
+    await this.remote.acessor(url, obj).then((res: any) =>{
+      if(res.auth == true &&  res.resp && !res.message){  
+        this.respostaData.aceitas = res.resp.aceitas.quantidadeEncomendasAceitas;
+        this.respostaData.recusadas = res.resp.recusadas.quantidadeEncomendasRecusadas;
+        this.respostaData.feitas = res.resp.feitas.quantidadeEncomendasFeitas;
+        this.respostaData.entregues = res.resp.entregues.quantidadeEncomendasEntregues;
+        this.respostaData.totalVendidos = res.resp.totalSalgadosVendidos;
+        console.log(res.resp);
+        
+      }else{
+        
+      }
+    })
+
+    var myChart = new Chart("myChart", {
+      type: 'bar',
+      data: {
+        labels: ['Aceitas', 'Recusadas', 'Feitas', 'Entregues', 'Total salgados vendidos'],
+        datasets: [{
+          label: 'Grafico de encomendas do mês de ' +  monthNames[d.getMonth()],
+          data: [this.respostaData.aceitas, this.respostaData.recusadas, this.respostaData.feitas, this.respostaData.entregues, this.respostaData.totalVendidos],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
-
 }
